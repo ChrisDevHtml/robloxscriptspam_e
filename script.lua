c878fec67de31d9c3a27a46f6e4576ab25301eb1
ChrisDevHtml <123667787+ChrisDevHtml@users.noreply.github.com>
2023-04-21 20:29:38 +0300

    Update script.lua

--- script.lua
+++ script.lua
@@ -1,4 +1,5 @@
 -- Credits to Temp#7031 he cool
+-- Fun fact: It only works on Slap battles roblox and coil hand!
 local key = game:GetService("UserInputService")
 key.InputBegan:Connect(function(input)
   if input.KeyCode == Enum.KeyCode.E then
